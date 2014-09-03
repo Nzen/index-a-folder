307cfe90d2f4d6259512acf96085b85a745cd031
Nicholas Prado <damiengrey@sbcglobal.net>
2014-09-03 13:22:53 -0700

    Basically works, BUT
    This now outputs links to the files, with the hard assumption that the folder structure is only one deep. That fits my current purpose, but will break if it is two deep. I'm done for now, but intend to make it more general later.

--- index_it.py
+++ index_it.py
@@ -19,15 +19,23 @@
 
 import os
 
-def folder_name( here ) :
+def just_last( here ) :
 	segment = here.split( '\\' )
 	if len( segment[0] ) == len( here ) :
 		segment = here.split( '/' ) # a small concession to _nix file slash
-	return segment[ -1 ] # last one is this folder
+	return segment[ -1 ] # last one is the goal
+
+def just_name( file_w_ext ) :
+	segment = file_w_ext.split( '.' )
+	return segment[ 0 ]
+
+def fill_with_files( output, files, here ) :
+	for curr_file in files :
+		output.write( "\n\t\t\t<li> <a href=\""+here+"\\"+curr_file+"\">"+just_name(curr_file)+"</a> </li>\n" )
+		# make these links, yay!
 
 def fill_with_subfolders( output, here ) :
 	f_depth = 2
-	curr_file = "basic I/O, world"
 	begin_folder = "\n\t<p>"
 	mid_folder = "\n\t\t <ul>"
 	end_folder = "\t\t </ul>\n\t</p>\n"
@@ -37,9 +45,9 @@ def fill_with_subfolders( output, here ) :
 		if current_f == here :
 			return
 		output.write( begin_folder )
-		output.write( "\n\t\t<h"+str(f_depth)+">" + folder_name(current_f) + "</h"+str(f_depth)+">" )
+		output.write( "\n\t\t<h"+str(f_depth)+">" + just_last(current_f) + "</h"+str(f_depth)+">" )
 		output.write( mid_folder )
-		output.write( "\n\t\t\t<li>" + curr_file + "</li>\n" )
+		fill_with_files( output, directory_tuple[2], just_last(current_f) ) # FIX assumption, only one folder deep
 		output.write( end_folder )
 		
 
@@ -51,31 +59,16 @@ def main() :
 	mid_htm +=		"</div><div class=\"content\">"
 	end_htm = "\n  </div>\n</body>\n</html>"
 
-	'''
-	make a file
-	put in begin, get foldername, put in mid
-	walk the structure, output the subfolders in their own paragraph
-	put in end
-	close the file
-	'''
 	here = os.getcwd()
 	output = open( "index.htm", 'w' )
 	output.write( begin_htm )
-	curr = folder_name( here )
+	curr = just_last( here )
 	output.write( curr ) # current folder is the title ; later get just the end
 	output.write( mid_htm )
 	output.write( "\n<h1>" + curr + "</h1>" )
+	#
 	fill_with_subfolders( output, here )
 	output.write( end_htm )
 	output.close()
 
 main()
-'''
-	the_files = "\\*"
-	for directory_tuple in os.walk( here, False ) : # top dir, start from bottom?
-		current = directory_tuple[0]
-		# well, one problem is the spaces aren't being eaten like I expect :\
-		if current == here :
-			return
-		for_bash = command + '\"' + current + the_files + '\"'
-'''
