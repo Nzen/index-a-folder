23b035ae9e2288c3792887c9ab24726c5291a8e3
Nicholas Prado <damiengrey@sbcglobal.net>
2014-09-03 12:49:58 -0700

    Fills body with folder names
    This actually walks the folders now and puts them in h2 tags. Next, put the file names and check if the depth changes.

--- index_it.py
+++ index_it.py
@@ -1,5 +1,5 @@
 '''
-<!DOCTYPE html>
+<!DOCTYPE html5>
 <html language='en'>
 <head>
 	<meta charset="UTF-8"></meta>
@@ -19,19 +19,37 @@
 
 import os
 
-def just_curr_folder_name( here ) :
+def folder_name( here ) :
 	segment = here.split( '\\' )
 	if len( segment[0] ) == len( here ) :
 		segment = here.split( '/' ) # a small concession to _nix file slash
 	return segment[ -1 ] # last one is this folder
 
 def fill_with_subfolders( output, here ) :
-	output.write( "\t<p>basic I/O, world</p>" )
+	f_depth = 2
+	curr_file = "basic I/O, world"
+	begin_folder = "\n\t<p>"
+	mid_folder = "\n\t\t <ul>"
+	end_folder = "\t\t </ul>\n\t</p>\n"
+	
+	for directory_tuple in os.walk( here, False ) : # top dir, start from bottom?
+		current_f = directory_tuple[0]
+		if current_f == here :
+			return
+		output.write( begin_folder )
+		output.write( "\n\t\t<h"+str(f_depth)+">" + folder_name(current_f) + "</h"+str(f_depth)+">" )
+		output.write( mid_folder )
+		output.write( "\n\t\t\t<li>" + curr_file + "</li>\n" )
+		output.write( end_folder )
+		
 
 def main() :
-	begin_htm = "<!DOCTYPE html>\n<html language='en'>\n<head>\n\t<meta charset=\"UTF-8\"></meta>\n\t<title>"
-	mid_htm = "</title>\n\t<link rel=\"stylesheet\" href=\"basic.css\">\n</head>\n<body>\n"
-	end_htm = "</body>\n</html>"
+	begin_htm = "<!DOCTYPE html5>\n<html language='en'>\n<head>\n\t<meta charset=\"UTF-8\"></meta>\n\t<title>"
+	mid_htm = "</title>\n\t<link rel=\"stylesheet\" href=\"basic.css\">\n</head>\n"
+	mid_htm +=		"<body>\n  <div class=\"twenty_p\">\n"
+	mid_htm +=		"<p>&emsp;</p>\n\n" * 10
+	mid_htm +=		"</div><div class=\"content\">"
+	end_htm = "\n  </div>\n</body>\n</html>"
 
 	'''
 	make a file
@@ -43,42 +61,21 @@ def main() :
 	here = os.getcwd()
 	output = open( "index.htm", 'w' )
 	output.write( begin_htm )
-	curr = just_curr_folder_name( here )
+	curr = folder_name( here )
 	output.write( curr ) # current folder is the title ; later get just the end
 	output.write( mid_htm )
-	output.write( "<h1>" + curr + "</h1>" )
+	output.write( "\n<h1>" + curr + "</h1>" )
 	fill_with_subfolders( output, here )
 	output.write( end_htm )
 	output.close()
 
-	'''
-	for directory_tuple in os.walk( here, False ) : # top dir, start from bottom?
-		if current == here :
-			return'''
-
 main()
 '''
-	newFileName = originalFileName[ : -3 ] + "asm" # replaces .txt or similar
-	output = open( newFileName, 'w' )
-	output.truncate( ) # erase what's in there for safety
-	for nn in self.smlData :
-		output.write( str( nn ) + '\n' ) # consider outputting as a grid, as in showMem()?
-	output.close( )
-	return newFileName
-
-	command = "sdelete.exe "# or "echo " for testing
-	folder_flag = "-r "
-	here = os.getcwd()
 	the_files = "\\*"
-	current = ""
-	for_bash = ""
 	for directory_tuple in os.walk( here, False ) : # top dir, start from bottom?
 		current = directory_tuple[0]
 		# well, one problem is the spaces aren't being eaten like I expect :\
 		if current == here :
 			return
 		for_bash = command + '\"' + current + the_files + '\"'
-		os.system( for_bash )
-		for_bash = command + '\"' + current + '\"'
-		os.system( for_bash )
 '''
