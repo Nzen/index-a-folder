cfc5a578def13e94341ec5707b06aa5d56d9e749
Nicholas Prado <damiengrey@sbcglobal.net>
2014-09-03 15:24:21 -0700

    Works as advertised
    This now checks and emits links to arbitrary folder depths. (Just checked that with a less trivial example.) I can't promise what happens to the <h_> tags when you go below 5, though.
    
    Also added a note that this is mit license. To be honest, that's half the reason I bothered. I found another python version, but it was gpl. The other half was that it depended on other libraries.

--- index_it.py
+++ index_it.py
@@ -1,4 +1,67 @@
 '''
+Author: Nicholas Prado - Nzen
+License: MIT
+Purpose: Outputs an html index of the folders it is run within.
+'''
+
+import os
+
+def just_last( here ) :
+	segment = here.split( '\\' )
+	if len( segment[0] ) == len( here ) :
+		segment = here.split( '/' ) # a small concession to _nix file slash
+	return segment[ -1 ] # last one is the goal
+
+def just_name( file_w_ext ) :
+	segment = file_w_ext.split( '.' )
+	return segment[ 0 ]
+
+def fill_with_files( output, files, here, cut_ind ) :
+	for curr_file in files :
+		rel_path = here[ cut_ind+1: ]
+		output.write( "\n\t\t\t<li> <a href=\""+rel_path+"\\"+curr_file+"\">"+just_name(curr_file)+"</a> </li>\n" )
+
+def fill_with_subfolders( output, here ) :
+	begin_folder = "\n\t<p>"
+	mid_folder = "\n\t\t <ul>"
+	end_folder = "\t\t </ul>\n\t</p>\n"
+	for directory_tuple in os.walk( here, True ) : # top dir, start from bottom?
+		current_f = directory_tuple[0]
+		#if current_f.find("ui source")>=0 : # a folder you don't need to traverse
+		#	continue
+		output.write( begin_folder )
+		rel_path = directory_tuple[0][ len(here)+1: ]
+		h_tag = rel_path.count( "\\" ) + 1
+		output.write( "\n\t\t<h"+str(h_tag)+">" + rel_path + "</h"+str(h_tag)+">" )
+		output.write( mid_folder )
+		fill_with_files( output, directory_tuple[2], directory_tuple[0], len(here) )
+		output.write( end_folder )
+		
+
+def main() :
+	begin_htm = "<!DOCTYPE html>\n<html language='en'>\n<head>\n\t<meta charset=\"UTF-8\"></meta>\n\t<title>"
+	mid_htm = "</title>\n\t<link rel=\"stylesheet\" href=\"basic.css\">\n</head>\n"
+	mid_htm +=		"<body>\n  <div class=\"twenty_p\">\n"
+	mid_htm +=		"<p>&emsp;</p>\n</div><div class=\"content\">"
+	end_htm = "\n  </div>\n</body>\n</html>"
+
+	here = os.getcwd()
+	output = open( "index.htm", 'w' )
+	output.write( begin_htm )
+	curr = just_last( here )
+	output.write( curr ) # current folder is the title ; later get just the end
+	output.write( mid_htm )
+	output.write( "\n<h1>" + curr + "</h1>" )
+	#
+	fill_with_subfolders( output, here )
+	output.write( end_htm )
+	output.close()
+
+main()
+
+'''
+	OUTPUT : index.htm
+
 <!DOCTYPE html5>
 <html language='en'>
 <head>
@@ -16,59 +79,3 @@
 </body>
 </html>
 '''
-
-import os
-
-def just_last( here ) :
-	segment = here.split( '\\' )
-	if len( segment[0] ) == len( here ) :
-		segment = here.split( '/' ) # a small concession to _nix file slash
-	return segment[ -1 ] # last one is the goal
-
-def just_name( file_w_ext ) :
-	segment = file_w_ext.split( '.' )
-	return segment[ 0 ]
-
-def fill_with_files( output, files, here ) :
-	for curr_file in files :
-		output.write( "\n\t\t\t<li> <a href=\""+here+"\\"+curr_file+"\">"+just_name(curr_file)+"</a> </li>\n" )
-		# make these links, yay!
-
-def fill_with_subfolders( output, here ) :
-	f_depth = 2
-	begin_folder = "\n\t<p>"
-	mid_folder = "\n\t\t <ul>"
-	end_folder = "\t\t </ul>\n\t</p>\n"
-	
-	for directory_tuple in os.walk( here, False ) : # top dir, start from bottom?
-		current_f = directory_tuple[0]
-		if current_f == here :
-			return
-		output.write( begin_folder )
-		output.write( "\n\t\t<h"+str(f_depth)+">" + just_last(current_f) + "</h"+str(f_depth)+">" )
-		output.write( mid_folder )
-		fill_with_files( output, directory_tuple[2], just_last(current_f) ) # FIX assumption, only one folder deep
-		output.write( end_folder )
-		
-
-def main() :
-	begin_htm = "<!DOCTYPE html5>\n<html language='en'>\n<head>\n\t<meta charset=\"UTF-8\"></meta>\n\t<title>"
-	mid_htm = "</title>\n\t<link rel=\"stylesheet\" href=\"basic.css\">\n</head>\n"
-	mid_htm +=		"<body>\n  <div class=\"twenty_p\">\n"
-	mid_htm +=		"<p>&emsp;</p>\n\n" * 10
-	mid_htm +=		"</div><div class=\"content\">"
-	end_htm = "\n  </div>\n</body>\n</html>"
-
-	here = os.getcwd()
-	output = open( "index.htm", 'w' )
-	output.write( begin_htm )
-	curr = just_last( here )
-	output.write( curr ) # current folder is the title ; later get just the end
-	output.write( mid_htm )
-	output.write( "\n<h1>" + curr + "</h1>" )
-	#
-	fill_with_subfolders( output, here )
-	output.write( end_htm )
-	output.close()
-
-main()
